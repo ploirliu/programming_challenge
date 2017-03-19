84a93b8cc601a757a65dbc335b25cca72e3a30f2
Ploir <ploirliu@163.com>
2017-03-19 16:35:14 +0800

    poj 1017 ac

--- programming_challenge/programming_challenge/P_2.2.5.h
+++ programming_challenge/programming_challenge/P_2.2.5.h
@@ -21,7 +21,7 @@ void solve(){
 	ll out = 0;
 	out += (ll)all[5];
 	
-	out += (ll)all[4]; all[0] = max(0, all[0] - 11*all[5]);
+	out += (ll)all[4]; all[0] = max(0, all[0] - 11*all[4]);
 	
 	out += (ll)all[3];
 	int tmp = all[3] * 5;
@@ -45,7 +45,7 @@ void solve(){
 		if (all[1]){
 			tmp2 = max(0, 3 - all[1]);
 			all[1] = max(0, all[1] - 3);
-			all[0] = max(0, all[0] - (18 - 4 * tmp2));
+			all[0] = max(0, all[0] - (6+4 * tmp2));
 		}
 		else{
 			all[0] = max(0, all[0] - 18);
@@ -55,7 +55,7 @@ void solve(){
 		if (all[1]){
 			tmp2 = max(0, 5 - all[1]);
 			all[1] = max(0, all[1] - 5);
-			all[0] = max(0, all[0] - (27 - 4 * tmp2));
+			all[0] = max(0, all[0] - (7+ 4 * tmp2));
 		}
 		else{
 			all[0] = max(0, all[0] - 27);
@@ -81,8 +81,71 @@ void solve(){
 }
 
 int main(){
-	while (scanf("%d %d %d %d %d %d", &all[0], &all[1], &all[2], &all[3], &all[4], &all[5])!=EOF){
+	//freopen("a.out", "w", stdout);
+	while (scanf("%d %d %d %d %d %d", &all[0], &all[1], &all[2], &all[3], &all[4], &all[5]) != EOF){
+		bool stu = true;
+		for (int i = 0; i < 6; ++i){
+			if (all[i] != 0){
+				stu = false;
+				break;
+			}
+		}
+		if (stu)
+			break;
 		solve();
 	}
 	return 0;
 }
+
+
+
+
+/*
+> 0 0 4 0 0 1                         2
+> 7 5 1 0 0 0						  > 1
+> 36 9 4 1 1 1						  > 6
+> 0 9 4 1 1 0						  > 4
+> 0 0 4 0 0 0						  > 1
+> 36 0 0 0 0 0						  > 1
+> 0 9 0 0 0 0						  > 1
+> 79 96 94 30 18 14					  > 86
+> 53 17 12 98 76 54					  > 231
+> 83 44 47 42 80 3					  > 137
+> 15 26 13 29 42 40					  > 115
+> 41 61 36 90 54 66					  > 219
+> 78 56 445 45 23 65				  > 245
+> 13 4 8 29 45 3					  > 79
+> 15 75 45 98 34 53					  > 197
+> 40 9 0 2 0 0						  > 3
+> 41 9 0 2 0 0						  > 4
+> 44 0 0 0 4 0						  > 4
+> 0 2 3 0 0 0						  > 2
+> 37 7 2 0 1 0						  > 3
+> 12 2 0 1 0 0						  > 1
+> 13 2 0 1 0 0						  > 2
+> 0 0 0 0 0 0
+
+
+2
+> 1
+> 6
+> 4
+> 1
+> 1
+> 1
+> 86
+> 231
+> 137
+> 115
+> 219
+> 245
+> 79
+> 197
+> 3
+> 4
+> 4
+> 2
+> 3
+> 1
+> 2
+*/
