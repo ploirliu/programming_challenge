4bd85a412a1ccef22cd7d72c9dee97e42f7fc4b8
Ploir <ploirliu@163.com>
2017-03-20 22:01:37 +0800

    Protecting the Flowers not ac

--- programming_challenge/programming_challenge/p_2.2.7.h
+++ programming_challenge/programming_challenge/p_2.2.7.h
@@ -16,59 +16,27 @@ const int INF = 1147483647;
 typedef long long ll;
 
 const int MAX_N = 105;
-double all[MAX_N];
-bool stu[MAX_N];
+int all[MAX_N];
 int n;
 
-bool getMin(double &num){
-	num = INF;
-	int id = -1;
-	bool out = false;
-	for (int i = 0; i < n; ++i){
-		if (stu[i]){
-			if (all[i] < num){
-				num = all[i];
-				id = i;
-				out = true;
-			}
-		}
-	}
-	if (out){
-		stu[id] = false;
-	}
-	return out;
-}
-
-bool getMax(double &num){
-	num = -INF;
-	int id = -1;
-	bool out = false;
-	for (int i = 0; i < n; ++i){
-		if (stu[i]){
-			if (all[i] > num){
-				num = all[i];
-				id = i;
-				out = true;
-			}
-		}
-	}
-	if (out){
-		stu[id] = false;
-	}
-	return out;
-}
-
 void solve(){
-	fill(stu, stu + n, true);
-	
+	priority_queue<double> q;
+	for (int i = 0; i < n; ++i){
+		q.push((double)all[i]);
+	}
+	while (q.size()>1){
+		double a = q.top(); q.pop();
+		double b = q.top(); q.pop();
+		double c = 2.0*sqrt(a*b);
+		q.push(c);
+	}
+	printf("%.3lf", q.top());
 }
 
 int main(){
 	while (scanf("%d", &n) != EOF){
-		int tmp;
 		for (int i = 0; i < n; ++i){
-			scanf("%d", &tmp);
-			all[i] = tmp;
+			scanf("%d", &all[i]);
 		}
 		solve();
 	}
---
+++ programming_challenge/programming_challenge/p_2.2.8.h
@@ -0,0 +1,59 @@
+#include <cstdio>
+#include <queue>
+#include <string>
+#include <set>
+#include <map>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <bitset>
+#include <cmath>
+#include <algorithm>
+#include <functional>
+using namespace std;
+const int INF = 1147483647;
+typedef long long ll;
+
+const int MAX_N = 100005;
+typedef struct node{
+	int cost, t;
+	bool operator<(const struct node &right)const{
+		if (cost == right.cost){
+			return t > right.t;
+		}
+		return cost < right.cost;
+	}
+}node;
+
+node all[MAX_N];
+int n;
+
+void solve(){
+	priority_queue<node> q;
+	ll out = 0,cost=0;
+	for (int i = 0; i < n; ++i){
+		q.push(all[i]);
+		cost += (ll)all[i].cost;
+	}
+	while (!q.empty()){
+		node p = q.top(); q.pop();
+		//cout << p.cost << " " << p.t << endl;
+		cost -= (ll)p.cost;
+		out += (ll)cost*2*p.t;
+		//cout << "out1:" << out << endl;
+		//out += (ll)p.cost*p.t;
+		//cout << "out2:" << out << endl;
+	}
+	printf("%lld\n", out);
+}
+
+int main(){
+	while (scanf("%d", &n) != EOF){
+		for (int i = 0; i < n; ++i){
+			scanf("%d %d", &all[i].t, &all[i].cost);
+		}
+		solve();
+	}
+	return 0;
+}
