2256842fc1d993021a26444164c8bd637e151bfb
liubo <liubo@haomaiyi.com>
2017-04-13 18:40:25 +0800

    2.3.4 just write

---
+++ programming_challenge/programming_challenge/p_2.3.4.h
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <queue>
+#include <string>
+#include <set>
+#include <map>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <bitset>
+#include <cmath>
+#include <algorithm>
+#include <functional>
+#include <string.h>
+using namespace std;
+const int INF = 1147483647;
+typedef long long ll;
+
+const int MAX_N = 1000005;
+const int MAX_M
+void solve(){
+
+}
+
+int main(){
+	while (scanf("", ) != EOF){
+
+		solve();
+	}
+	return 0;
+}
