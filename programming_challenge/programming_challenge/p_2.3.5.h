78077cce5361c0562bbda2e0ad3e579c58408016
liubo <liubo@haomaiyi.com>
2017-04-19 10:52:04 +0800

    poj 1742 time out

--- programming_challenge/programming_challenge/p_2.3.5.h
+++ programming_challenge/programming_challenge/p_2.3.5.h
@@ -24,8 +24,8 @@ typedef struct node{
 }node;
 
 const int len = 2015;
-char s1[len];
-char s2[len];
+char s[len];
+//char s2[len];
 node all[30];
 
 int cost[len][len];
@@ -43,9 +43,9 @@ int cost[len][len];
 //	return 1;
 //}
 
-inline int id(int i){
-	return i - 1;
-}
+//inline int id(int i){
+//	return i - 1;
+//}
 
 //void solve(){
 //	memset(cost, 0, sizeof(cost));
@@ -62,37 +62,77 @@ inline int id(int i){
 //	printf("%d\n", cost[id(m - 1)][id(0)]);
 //}
 
+//void show(){
+//	for (int i = 0; i <= (m + 1); ++i){
+//		for (int j = 0; j <= (m + 1); ++j){
+//			cout << cost[i][j] << ' ';
+//		}
+//		cout << endl;
+//	}
+//}
+//
+//void solve(){
+//	memset(cost, 0, sizeof(cost));
+//	for (int j = m + 1; j > 0; --j){
+//		if (j != (m + 1)){
+//			cost[0][j] = cost[0][j + 1] + min(all[s1[id(j)]].add, all[s1[id(j)]].sub);
+//		}
+//	}
+//	for (int i = 1; i <= m; ++i){
+//		for (int j = m + 1; j > 0; --j){
+//			if (j == (m + 1)){
+//				cost[i][j] = cost[i - 1][j] + min(all[s1[id(i)]].add, all[s1[id(i)]].sub);
+//			}
+//			else if (s1[id(i)] == s1[id(j)]){
+//				cost[i][j] = cost[i-1][j+1];
+//			}
+//			else{
+//				cost[i][j] = min(cost[i - 1][j] + min(all[s1[id(i)] - 'a'].add, all[s1[id(i)] - 'a'].sub), cost[i][j + 1] + min(all[s1[id(j)] - 'a'].add, all[s1[id(j)] - 'a'].sub));
+//			}
+//		}
+//	}
+//	//show();
+//	printf("%d\n", cost[m][1]);
+//}
+
 void show(){
-	for (int i = 0; i <= (m + 1); ++i){
-		for (int j = 0; j <= (m + 1); ++j){
+	for (int i = 0; i <= m; ++i){
+		for (int j = 0; j <= m; ++j){
 			cout << cost[i][j] << ' ';
 		}
 		cout << endl;
 	}
 }
 
+inline int f_id(int i){
+	return i - 1;
+}
+inline int b_id(int i){
+	return m - i;
+}
 void solve(){
 	memset(cost, 0, sizeof(cost));
-	for (int j = m + 1; j > 0; --j){
-		if (j != (m + 1)){
-			cost[0][j] = cost[0][j + 1] + min(all[s1[id(j)]].add, all[s1[id(j)]].sub);
-		}
-	}
-	for (int i = 1; i <= m; ++i){
-		for (int j = m + 1; j > 0; --j){
-			if (j == (m + 1)){
-				cost[i][j] = cost[i - 1][j] + min(all[s1[id(i)]].add, all[s1[id(i)]].sub);
+	for (int i = 0; i <= m; ++i){
+		for (int j = 0; j <= m; ++j){
+			if (i == 0 && j == 0)
+				continue;
+			else if (i == 0){
+				cost[i][j] = cost[i][j - 1] + min(all[s[b_id(j)] - 'a'].add, all[s[b_id(j)] - 'a'].sub);
 			}
-			else if (s1[id(i)] == s1[id(j)]){
-				cost[i][j] = cost[i-1][j+1];
+			else if (j == 0){
+				cost[i][j] = cost[i - 1][j] + min(all[s[f_id(i)] - 'a'].sub, all[s[f_id(i)] - 'a'].add);
+			}
+			else if (s[f_id(i)] == s[b_id(j)]){
+				cost[i][j] = cost[i - 1][j - 1];
 			}
 			else{
-				cost[i][j] = min(cost[i - 1][j] + min(all[s1[id(i)] - 'a'].add, all[s1[id(i)] - 'a'].sub), cost[i][j + 1] + min(all[s1[id(j)] - 'a'].add, all[s1[id(j)] - 'a'].sub));
+				cost[i][j] = min(cost[i - 1][j] + min(all[s[f_id(i)] - 'a'].sub, all[s[f_id(i)] - 'a'].add),
+					cost[i][j - 1] + min(all[s[b_id(j)] - 'a'].add, all[s[b_id(j)] - 'a'].sub));
 			}
 		}
 	}
 	//show();
-	printf("%d\n", cost[m][1]);
+	printf("%d\n", cost[m][m]/2);
 }
 
 int main(){
@@ -100,7 +140,7 @@ int main(){
 	while (scanf("%d %d",&n,&m ) != EOF){
 		//s = new char[m+1];
 	//while (true){
-		scanf("%s", &s1);
+		scanf("%s", &s);
 		char c;
 		for (int i = 0; i < n; ++i){
 			scanf("%c", &c);
---
+++ programming_challenge/programming_challenge/p_2.4.1.h
@@ -0,0 +1,114 @@
+#include <cstdio>
+#include <queue>
+#include <string>
+#include <set>
+#include <map>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <bitset>
+#include <cmath>
+#include <algorithm>
+#include <functional>
+#include <string.h>
+using namespace std;
+const int INF = 1147483647;
+typedef long long ll;
+
+map<int, int> v;
+int n, m;
+
+const int MAX_N = 105;
+const int MAX_M = 100005;
+int value[MAX_N];
+int num[MAX_N];
+
+//bool dp[2][MAX_M];
+bool dp[MAX_M];
+
+set<ll> cache;
+void show(){
+	for (set<ll>::iterator i = cache.begin(); i != cache.end();++i){
+		cout << *i <<" ";
+	}
+	cout << endl;
+}
+void dfs(ll now){
+	if (now != 0 && now<=(ll)m)
+		cache.insert(now);
+	if (now > (ll)m)
+		return;
+	for (int i = 0; i < n; ++i){
+		if (num[i]){
+			--num[i];
+			dfs(now + (ll)value[i]);
+			++num[i];
+		}
+	}
+}
+
+void solve(){
+	cache.clear();
+	dfs(0);
+	show();
+	printf("%d\n", cache.size());
+}
+
+void solve_dp(){
+	memset(dp, 0, sizeof(dp));
+	//dp[0][0] = true;
+	//dp[1][0] = true;
+	dp[0] = true;
+	for (int i = 0; i < n; ++i){
+		//for (int j = 1; j <= num[i]; ++j){
+		//	for (int k = m; k >=1; --k){
+		//		int tmp = (k - j*value[i]);
+		//		if (tmp < 0)
+		//			break;
+		//		if (dp[tmp])
+		//			dp[k] = true;
+		//	}
+		//}
+		for (int k = m; k >=1; --k){
+			for (int j = 1; j <=num[i]; ++j){
+				int tmp = (k - j*value[i]);
+				if (tmp < 0)
+					break;
+				if (dp[tmp])
+					dp[k] = true;
+			}
+		}
+
+
+
+
+		//cout << i << endl;
+		//for (int k = 0; k <= m; ++k)
+		//	cout << dp[k] << ' ';
+		//cout << endl;
+	}
+	int ans = 0;
+	for (int i = 1; i <= m; ++i){
+		if (dp[i])
+			++ans;
+	}
+	printf("%d\n", ans);
+}
+
+int main(){
+	//freopen("a.in", "r", stdin);
+	while (scanf("%d %d",&n,&m ) != EOF){
+		if (m == 0 && n == 0)
+			break;
+		v.clear();
+		for (int i = 0; i < n; ++i){
+			scanf("%d", &value[i]);
+		}
+		for (int i = 0; i < n; ++i){
+			scanf("%d", &num[i]);
+		}
+		solve_dp();
+	}
+	return 0;
+}
