cef684cf0130024c0b0a4e26e0cc6c10a1b0ba73
ploirliu <ploirliu@163.com>
2017-04-21 21:29:48 +0800

    poj 1742 success

--- programming_challenge/programming_challenge/p_2.4.1.h
+++ programming_challenge/programming_challenge/p_2.4.1.h
@@ -25,76 +25,108 @@ int value[MAX_N];
 int num[MAX_N];
 
 //bool dp[2][MAX_M];
-bool dp[MAX_M];
+int dp[MAX_M];
 
-set<ll> cache;
+//set<ll> cache;
+//void show(){
+//	for (set<ll>::iterator i = cache.begin(); i != cache.end();++i){
+//		cout << *i <<" ";
+//	}
+//	cout << endl;
+//}
+//void dfs(ll now){
+//	if (now != 0 && now<=(ll)m)
+//		cache.insert(now);
+//	if (now > (ll)m)
+//		return;
+//	for (int i = 0; i < n; ++i){
+//		if (num[i]){
+//			--num[i];
+//			dfs(now + (ll)value[i]);
+//			++num[i];
+//		}
+//	}
+//}
+//
+//void solve(){
+//	cache.clear();
+//	dfs(0);
+//	show();
+//	printf("%d\n", cache.size());
+//}
+//
+//void solve_dp(){
+//	memset(dp, 0, sizeof(dp));
+//	//dp[0][0] = true;
+//	//dp[1][0] = true;
+//	dp[0] = true;
+//	for (int i = 0; i < n; ++i){
+//		//for (int j = 1; j <= num[i]; ++j){
+//		//	for (int k = m; k >=1; --k){
+//		//		int tmp = (k - j*value[i]);
+//		//		if (tmp < 0)
+//		//			break;
+//		//		if (dp[tmp])
+//		//			dp[k] = true;
+//		//	}
+//		//}
+//		for (int k = m; k >=1; --k){
+//			for (int j = 1; j <=num[i]; ++j){
+//				int tmp = (k - j*value[i]);
+//				if (tmp < 0)
+//					break;
+//				if (dp[tmp])
+//					dp[k] = true;
+//			}
+//		}
+//
+//
+//
+//
+//		//cout << i << endl;
+//		//for (int k = 0; k <= m; ++k)
+//		//	cout << dp[k] << ' ';
+//		//cout << endl;
+//	}
+//	int ans = 0;
+//	for (int i = 1; i <= m; ++i){
+//		if (dp[i])
+//			++ans;
+//	}
+//	printf("%d\n", ans);
+//}
+
 void show(){
-	for (set<ll>::iterator i = cache.begin(); i != cache.end();++i){
-		cout << *i <<" ";
+	for (int i = 0; i <= m; ++i){
+		cout << dp[i] << ' ';
 	}
 	cout << endl;
 }
-void dfs(ll now){
-	if (now != 0 && now<=(ll)m)
-		cache.insert(now);
-	if (now > (ll)m)
-		return;
-	for (int i = 0; i < n; ++i){
-		if (num[i]){
-			--num[i];
-			dfs(now + (ll)value[i]);
-			++num[i];
-		}
-	}
-}
-
 void solve(){
-	cache.clear();
-	dfs(0);
-	show();
-	printf("%d\n", cache.size());
-}
-
-void solve_dp(){
-	memset(dp, 0, sizeof(dp));
-	//dp[0][0] = true;
-	//dp[1][0] = true;
-	dp[0] = true;
+	memset(dp, -1, sizeof(dp));
+	dp[0] = 0;
 	for (int i = 0; i < n; ++i){
-		//for (int j = 1; j <= num[i]; ++j){
-		//	for (int k = m; k >=1; --k){
-		//		int tmp = (k - j*value[i]);
-		//		if (tmp < 0)
-		//			break;
-		//		if (dp[tmp])
-		//			dp[k] = true;
-		//	}
-		//}
-		for (int k = m; k >=1; --k){
-			for (int j = 1; j <=num[i]; ++j){
-				int tmp = (k - j*value[i]);
-				if (tmp < 0)
-					break;
-				if (dp[tmp])
-					dp[k] = true;
+		for (int j = 0; j <= m; ++j){
+			if (dp[j] >= 0){
+				dp[j] = num[i];
 			}
+			else if (j < value[i] || dp[j - value[i]] <= 0){
+				dp[j] = -1;
+			}
+			else{
+				dp[j] = dp[j - value[i]] - 1;
+			}
+			//show();
 		}
-
-
-
-
-		//cout << i << endl;
-		//for (int k = 0; k <= m; ++k)
-		//	cout << dp[k] << ' ';
-		//cout << endl;
 	}
 	int ans = 0;
 	for (int i = 1; i <= m; ++i){
-		if (dp[i])
+		if (dp[i] >= 0)
 			++ans;
 	}
 	printf("%d\n", ans);
 }
+
 
 int main(){
 	//freopen("a.in", "r", stdin);
@@ -108,7 +140,7 @@ int main(){
 		for (int i = 0; i < n; ++i){
 			scanf("%d", &num[i]);
 		}
-		solve_dp();
+		solve();
 	}
 	return 0;
 }
