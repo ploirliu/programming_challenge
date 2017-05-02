040c484307044a71e3a568d0d008a0f627203ec0
liubo <liubo@haomaiyi.com>
2017-05-02 14:23:35 +0800

    aoj 0189: Convenient Location not success

--- programming_challenge/programming_challenge/p_2.8.1.h
+++ programming_challenge/programming_challenge/p_2.8.1.h
@@ -18,8 +18,8 @@ typedef long long ll;
 typedef pair<int, int> P;
 #define rep(i,start,end) for(int i=(start);i<(end);++i)
 #define pint(i) printf("%d\n",(i))
-#define pint2(i,j) printf("%d %d",(i),(j))
-#define pint3(i,j,k) printf("%d %d %d",(i),(j),(k))
+#define pint2(i,j) printf("%d %d\n",(i),(j))
+#define pint3(i,j,k) printf("%d %d %d\n",(i),(j),(k))
 #define gint(i) scanf("%d",&(i))
 #define gint2(i,j) scanf("%d %d",&(i),&(j))
 #define gint3(i,j,k) scanf("%d %d %d",&(i),&(j),&(k))
@@ -33,7 +33,7 @@ typedef pair<int, int> P;
 
 class bignum{
 public:
-	bignum(int size = 4):len(size){
+	bignum(int size = 4) :len(size){
 	}
 	~bignum(){
 	}
@@ -73,13 +73,13 @@ public:
 		return *this;
 	}
 	const bignum& operator=(const bignum&right){
-		for (int i = 0; i <len; ++i){
+		for (int i = 0; i < len; ++i){
 			num[i] = right.num[i];
 		}
 		return *this;
 	}
 	void set(int a){
-		for (int i = 0; i <len; ++i){
+		for (int i = 0; i < len; ++i){
 			num[i] = a%gap;
 			a /= gap;
 		}
@@ -87,7 +87,7 @@ public:
 private:
 	int  num[4];
 	int len;
-	static const int gap =1000000000;
+	static const int gap = 1000000000;
 };
 
 
@@ -107,18 +107,66 @@ private:
 //}
 
 
-#define my_debug
+//#define my_debug
+
+const int MAX_NUM = 15;
+
+int n;
+int len[MAX_NUM][MAX_NUM];
+int stu[MAX_NUM];
+set<int> cache;
+void myinit(){
+	rep(i, 0, MAX_NUM)
+		rep(j, 0, MAX_NUM){
+		if (i == j)
+			len[i][j] = 0;
+		else
+			len[i][j] = INF;
+	}
+}
+
 
 void solve(){
-	
+	rep(i, 0, MAX_NUM){
+		rep(j, 0, MAX_NUM){
+			rep(k, 0, MAX_NUM){
+				len[i][j] = min(len[i][j], len[i][k] + len[k][j]);
+			}
+		}
+	}
+	int out = INF, t = 0;
+	for (int i : cache){
+		int tmp = 0;
+		for (int j : cache){
+			tmp += len[i][j];
+		}
+		if (tmp < out){
+			out = tmp;
+			t = i;
+		}
+	}
+	pint2(t, out);
 }
 
 int main(){
 #ifdef my_debug
 	freopen("a.in", "r", stdin);
 #endif
-    while (gint() != EOF){
-		
+	while (gint(n) != EOF){
+		if (n == 0)
+			break;
+		fill(stu, stu + MAX_NUM, INF);
+		myinit();
+		cache.clear();
+		int tmp1, tmp2;
+		rep(i, 0, n){
+			gint2(tmp1, tmp2);
+			int tmp;
+			gint(tmp);
+			len[tmp2][tmp1] = len[tmp1][tmp2] = tmp;
+			cache.insert(tmp1);
+			cache.insert(tmp2);
+		}
 		solve();
 	}
 	return 0;
--- programming_challenge/programming_challenge/programming_challenge.cpp
+++ programming_challenge/programming_challenge/programming_challenge.cpp
@@ -2,7 +2,7 @@
 //
 
 #include "stdafx.h"
-#include "p_2.7.3.h"
+#include "p_2.8.1.h"
 
 int _tmain(int argc, _TCHAR* argv[])
 {
