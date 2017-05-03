4ba6272c9d84e350ccd3a32f51d277735b169f25
ploirliu <ploirliu@163.com>
2017-05-03 22:03:32 +0800

    shortest path not success

--- programming_challenge/programming_challenge/p_2.8.2.h
+++ programming_challenge/programming_challenge/p_2.8.2.h
@@ -13,20 +13,20 @@
 #include <functional>
 #include <string.h>
 using namespace std;
-const int INF = 1000000000;
+const int INF = 1 << 28;
 typedef long long ll;
 typedef pair<int, int> P;
 #define rep(i,start,end) for(int i=(start);i<(end);++i)
 #define pint(i) printf("%d\n",(i))
-#define pint2(i,j) printf("%d %d\n",(i),(j))
-#define pint3(i,j,k) printf("%d %d %d\n",(i),(j),(k))
+#define pint2(i,j) printf("%d %d",(i),(j))
+#define pint3(i,j,k) printf("%d %d %d",(i),(j),(k))
 #define gint(i) scanf("%d",&(i))
 #define gint2(i,j) scanf("%d %d",&(i),&(j))
 #define gint3(i,j,k) scanf("%d %d %d",&(i),&(j),&(k))
 #define init(tar,val) memset((tar),(val),sizeof((tar)))
 #define show(tar,len) for(int i=0;i<(len);++i) \
-	cout<<tar[i]<<' ';\
-	cout<<endl;
+	cout << tar[i] << ' '; \
+	cout << endl;
 
 
 
@@ -73,13 +73,13 @@ public:
 		return *this;
 	}
 	const bignum& operator=(const bignum&right){
-		for (int i = 0; i < len; ++i){
+		for (int i = 0; i <len; ++i){
 			num[i] = right.num[i];
 		}
 		return *this;
 	}
 	void set(int a){
-		for (int i = 0; i < len; ++i){
+		for (int i = 0; i <len; ++i){
 			num[i] = a%gap;
 			a /= gap;
 		}
@@ -107,72 +107,59 @@ private:
 //}
 
 
-//#define my_debug
+#define my_debug
+const int MAX_N = 305;
+int len[MAX_N][MAX_N];
+int n, m;
 
-const int MAX_NUM = 15;
-
-int n;
-int len[MAX_NUM][MAX_NUM];
-int stu[MAX_NUM];
-set<int> cache;
 void myinit(){
-	rep(i, 0, MAX_NUM)
-		rep(j, 0, MAX_NUM){
-		if (i == j)
-			len[i][j] = 0;
-		else
-			len[i][j] = INF;
+	rep(i, 0, MAX_N){
+		rep(j, 0, MAX_N){
+			len[i][j] = (i == j ? 0 : INF);
+		}
 	}
 }
 
-
 void solve(){
-	//rep(i, 0, MAX_NUM){
-	//	rep(j, 0, MAX_NUM){
-	//		rep(k, 0, MAX_NUM){
-	//			len[i][j] = min(len[i][j], len[i][k] + len[k][j]);
-	//		}
-	//	}
-	//}
-	for (int i : cache){
-		for (int j : cache){
-			for (int k : cache){
+	rep(i, 1, n + 1){
+		rep(j, 1, n + 1){
+			rep(k, 1, n + 1){
 				len[i][j] = min(len[i][j], len[i][k] + len[k][j]);
 			}
 		}
 	}
-	int out = INF, t = 0;
-	for (int i : cache){
+	int out = INF;
+	rep(i, 1, n + 1){
 		int tmp = 0;
-		for (int j : cache){
-			tmp += len[i][j];
+		rep(j, 1, n + 1){
+			if (i != j){
+				tmp += len[i][j];
+			}
 		}
-		if (tmp < out){
-			out = tmp;
-			t = i;
-		}
+		out = min(out, tmp);
 	}
-	pint2(t, out);
+	//double tmp = (double)0.5+(double)out * 100 / (n - 1);
+	pint(out * 100 / (n-1));
 }
 
 int main(){
 #ifdef my_debug
 	freopen("a.in", "r", stdin);
 #endif
-	while (gint(n) != EOF){
-		if (n == 0)
-			break;
-		fill(stu, stu + MAX_NUM, INF);
+	while (gint2(n,m) != EOF){
 		myinit();
-		cache.clear();
-		int tmp1, tmp2;
-		rep(i, 0, n){
-			gint2(tmp1, tmp2);
+		rep(i, 0, m){
 			int tmp;
 			gint(tmp);
-			len[tmp2][tmp1] = len[tmp1][tmp2] = tmp;
-			cache.insert(tmp1);
-			cache.insert(tmp2);
+			int now[MAX_N];
+			rep(j, 0, tmp){
+				gint(now[j]);
+			}
+			rep(j, 0, tmp){
+				rep(k, j + 1, tmp){
+					len[now[j]][now[k]] = len[now[k]][now[j]] = 1;
+				}
+			}
 		}
 		solve();
 	}
